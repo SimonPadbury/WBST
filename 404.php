abbc8300db8fc87003efa202a6ab3e20a84513dc
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-03-11 18:11:21 +0000

    Update 404.php

--- 404.php
+++ 404.php
@@ -5,7 +5,6 @@
     
     <div class="col-xs-12 col-sm-8">
       <div id="content" role="main">
-        <?php tha_content_before(); ?>
         <div class="alert alert-warning">
           <h1><i class="glyphicon glyphicon-warning-sign"></i> <?php _e('Error', 'wbst'); ?> 404</h1>
           <p><?php _e('The page you were looking for does not exist.', 'wbst'); ?></p>
