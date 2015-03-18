f7c3701763f48535d272371e6959c8ae2eabe85a
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-03-18 07:42:39 +0000

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php wp_title('•', true, 'right'); bloginfo('name'); ?></title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
 
 <!--[if lt IE 8]>
 <div class="alert alert-warning">
