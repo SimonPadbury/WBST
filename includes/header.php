270cbde6a44c0a7bee865953e18ddce91000003c
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-03-18 07:36:37 +0000

    Using Edward McIntire's wp_navwalker

--- includes/header.php
+++ includes/header.php
@@ -28,28 +28,24 @@
       <?php /* <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> */ ?>
     </div>
     <div class="collapse navbar-collapse" id="navbar-upper">
-      <?php				
-            $args = array(
-              'theme_location' 	=> 'navbar-upper-left',
-              'depth' 		=> 0,
-              'container'	=> false,
-              'fallback_cb' 	=> false,
-              'menu_class' 	=> 'nav navbar-nav',
-              'walker' 		=> new BootstrapNavMenuWalker()
+      <?php
+            wp_nav_menu( array(
+                'theme_location'    => 'navbar-upper-left',
+                'depth'             => 2,
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                'walker'            => new wp_bootstrap_navwalker())
             );
-            wp_nav_menu($args);
         ?>
         <?php get_template_part('includes/navbar-search'); ?>
-        <?php				
-            $args = array(
-              'theme_location' 	=> 'navbar-upper-right',
-              'depth' 		=> 0,
-              'container'	=> false,
-              'fallback_cb' 	=> false,
-              'menu_class' 	=> 'nav navbar-nav navbar-right',
-              'walker' 		=> new BootstrapNavMenuWalker()
+        <?php
+            wp_nav_menu( array(
+                'theme_location'    => 'navbar-upper-right',
+                'depth'             => 2,
+                'menu_class'        => 'nav navbar-nav navbar-right',
+                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                'walker'            => new wp_bootstrap_navwalker())
             );
-            wp_nav_menu($args);
         ?>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
@@ -77,27 +73,22 @@
         <?php /* <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> */ ?>
       </div><!-- /.navbar-header -->
       <div class="collapse navbar-collapse" id="navbar-lower">    
-        <?php				
-            $args = array(
-              'theme_location' 	=> 'navbar-lower-left',
-              'depth' 		=> 0,
-              'container'	=> false,
-              'fallback_cb' 	=> false,
-              'menu_class' 	=> 'nav navbar-nav',
-              'walker' 		=> new BootstrapNavMenuWalker()
+        <?php
+            wp_nav_menu( array(
+                'theme_location'    => 'navbar-lower-left',
+                'depth'             => 2,
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                'walker'            => new wp_bootstrap_navwalker())
             );
-            wp_nav_menu($args);
-        ?>
-        <?php				
-            $args = array(
-              'theme_location' 	=> 'navbar-lower-right',
-              'depth' 		=> 0,
-              'container'	=> false,
-              'fallback_cb' 	=> false,
-              'menu_class' 	=> 'nav navbar-nav navbar-right',
-              'walker' 		=> new BootstrapNavMenuWalker()
+        ?><?php
+            wp_nav_menu( array(
+                'theme_location'    => 'navbar-lower-right',
+                'depth'             => 2,
+                'menu_class'        => 'nav navbar-nav navbar-right',
+                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                'walker'            => new wp_bootstrap_navwalker())
             );
-            wp_nav_menu($args);
         ?>
       </div><!-- /.navbar-collapse -->
     </nav>
