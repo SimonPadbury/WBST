d9d2a1af6196f49e1aea0091a6746635dca2cfbc
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-12-09 10:13:26 +0000

    Update woocommerce-setup.php

--- functions/woocommerce-setup.php
+++ functions/woocommerce-setup.php
@@ -45,7 +45,7 @@ remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wra
 // Add BST Plus wrappers
 add_action('woocommerce_before_main_content', 'wbst_wrapper_start', 10);
 add_action('woocommerce_after_main_content', 'wbst_wrapper_end', 10);
-function reinnervate_wrapper_start() {
+function wbst_wrapper_start() {
   echo '<div id="content" class="site-content container">';
   echo '<div id="primary" class="content-area col-sm-12 col-md-8 '.of_get_option('site_layout').' ">';
   echo '<main id="main" class="site-main" role="main">';
